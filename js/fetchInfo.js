23940a4e0ea79f8cf705a71a228e29759aa800e2
davidahlstroem <david.ahlstroem@gmail.com>
2022-02-03 12:30:19 +0100

    Upload

---
+++ js/fetchInfo.js
@@ -0,0 +1,9 @@
+﻿export function getInfo() {
+    return fetch("https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18.1489/lat/57.3081/data.json")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            return data;
+        });
+}
---
+++ js/javascript.js
@@ -0,0 +1,36 @@
+﻿import { getInfo } from "./fetchInfo.js";
+
+createApi();
+
+function createApi() {
+    getInfo().then(insertInfo);
+}
+
+function insertInfo(result) {
+    //console.dir(result);
+    var currentdate = getCurrentDate();
+    var temp = 0;
+    for (var i = 0; i < result.timeSeries.length; i++) {
+        
+        for (var j = 0; j < result.timeSeries[i].parameters.length; j++) {
+            
+            if (result.timeSeries[i].validTime === currentdate && result.timeSeries[i].parameters[j].name === "t") {
+                temp = result.timeSeries[i].parameters[j].values[0];
+
+            }
+        } 
+    }
+    writeWeather(temp);
+}
+
+function writeWeather(temp) {
+    return document.getElementById("temp").innerHTML = `I Djupvikshamn är det under nästkommande timme ${temp} grader varmt.`;
+}
+
+function getCurrentDate() {
+    var ct = new Date();
+    var utc = new Date(ct.getTime() + ct.getTimezoneOffset());
+    utc.setUTCHours(utc.getUTCHours() + 3); // enkel fix
+    const datetime = utc.toISOString().split(':')[0] + ":00:00Z"; // lyckas inte få den rätt efter toISOString, två timmar fel, förklara gärna varför
+    return datetime;
+}
